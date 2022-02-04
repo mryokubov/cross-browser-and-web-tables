e41d7a91db1a9b3a6a6cf5bfb6537d1d93f51f71
TairovichJR <yokubov.omonjon@gmail.com>
2022-02-03 22:19:47 -0500

    iterate tables

--- src/test/java/tables/HtmlTableVerification.java
+++ src/test/java/tables/HtmlTableVerification.java
@@ -5,17 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import utils.ExcelReader;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by tairovich_jr on 2022-02-03.
@@ -70,44 +62,18 @@ public class HtmlTableVerification {
         driver.findElement(By.xpath("//a[text()='View all orders']")).click();
 
 
-        Object[][] customers = customers();
-
-        for (int i = 0; i < customers.length; i++) {
-            for (int j = 0; j < customers[i].length; j++) {
-                System.out.println(customers[i][j]);
-            }
-        }
-
-
         int rows = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr")).size(); //8
         int cols = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr[1]/th")).size();
 
-        System.out.println( "========================");
-        Object[][] actualData = new Object[rows-1][1];
-        for (int i = 2, k = 0; i <= rows; i++, k++) {
-            Map<String,String> map = new LinkedHashMap<>();
+        for (int i = 2; i <= rows; i++) {
             for (int j = 2; j <= cols-1 ; j++) {
 
                 WebElement cellData = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr[" +i+ "]/td["+ j + "]"));
-                WebElement header = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']/tbody/tr[" + 1 + "]/th[" + j + "]"));
-                map.put(header.getText(), cellData.getText());
+                System.out.print(cellData.getText() + " ");
             }
-            actualData[k][0] = map;
+            System.out.println();
         }
-
-        for (int i = 0; i < actualData.length; i++) {
-            for (int j = 0; j < actualData[i].length; j++) {
-                System.out.println(actualData[i][j]);
-            }
-        }
-
 
     }
 
-
- //   @DataProvider(name = "customers")
-    public Object[][] customers(){
-        ExcelReader reader = new ExcelReader("src/main/resources/testData/web-orders.xlsx","customers");
-        return reader.getData();
-    }
 }
